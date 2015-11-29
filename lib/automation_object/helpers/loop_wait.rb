0bfe355b5558977ffb0b016dc014a400d2c12649
Michael Blatter <mblatter@spartzinc.com>
2015-11-29 17:20:58 -0600

    Updates

--- lib/automation_object/helpers/loop_wait.rb
+++
@@ -1,8 +0,0 @@
-#Simple class for for looping through a condition until ready
-class LoopWait
-  def initialize(args = {})
-    @sleep = args.fetch :sleep
-    @loops = args.fetch :loops
-    @proc = args.fetch :proc
-  end
-end
