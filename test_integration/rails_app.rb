e2f12534c2fbff8830b0d5222dd3ec0cadf9402e
Mike Blatter <mike@blatter.me>
2016-11-07 12:09:52 -0600

    Updates

--- test_integration/rails_app.rb
+++ test_integration/rails_app.rb
@@ -20,6 +20,10 @@ class RailsApp
   end
 
   def create
+    `cd #{BASE_DIR}; bundle install`
+    `cd #{BASE_DIR}; bundle exec rake db:create`
+    `cd #{BASE_DIR}; bundle exec rake db:migrate`
+    
     @pid = Process.spawn("cd #{BASE_DIR}; rails s -p #{PORT} > /dev/null 2>&1")#
 
     10.times do
