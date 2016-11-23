4277e4c555af076ff86af13c2f8c721314255c0d
Mike Blatter <mblatter@MacBook-Pro-9.local>
2016-11-23 16:24:32 -0600

    Test updates

---
+++ test/automation_object/driver/common_selenium/element_geometry_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+require_relative '../../../test_helper'
+
+# Test AutomationObject::Driver::CommonSelenium::ElementGeometry
+class TestDriverCommonSeleniumDriver < Minitest::Test
+  def setup
+  end
+
+  def teardown
+  end
+
+end
---
+++ test/automation_object/driver/common_selenium/element_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+require_relative '../../../test_helper'
+
+# Test AutomationObject::Driver::CommonSelenium::Element
+class TestDriverCommonSeleniumElement < Minitest::Test
+  def setup
+  end
+
+  def teardown
+  end
+
+end
