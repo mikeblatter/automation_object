1ec917d125f7ea9072af7ef6820ab94c769ee232
Mike Blatter <mblatter@enova.com>
2017-06-29 13:58:23 -0500

    Add method missing error when calling non-existant method or child

--- lib/automation_object/dsl/_base.rb
+++ lib/automation_object/dsl/_base.rb
@@ -20,6 +20,15 @@ module AutomationObject
         super ostruct_hash
       end
 
+      # @param [Symbol] method
+      # @param [Array, nil] args
+      # @param [Proc] block
+      def method_missing(method, *args, &block)
+        return super if to_h.has_key?(method)
+
+        raise NoMethodError.new("undefined method '#{method}'", method, args)
+      end
+
       def to_s
         inspect
       end
